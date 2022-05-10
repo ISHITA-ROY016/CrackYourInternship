afcd4030e4fbf0b9884ccf56564ee52ca09db2b3
ISHITA ROY <75474643+ISHITA-ROY016@users.noreply.github.com>
2022-05-10 23:51:15 +0530

    Create best_time_to_buy_and_sell_stocks_II.java

---
+++ best_time_to_buy_and_sell_stocks_II.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+      int profit=0;
+      for(int i=1;i<n;i++){
+        profit+=Math.max(prices[i]-prices[i-1],0);
+      }
+      return profit;
+    }
+}
